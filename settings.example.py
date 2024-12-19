61b0f9aacb1891a4af7c78f59f2ccdab4613987a
Chris Cartwright <chris@chris-cartwright.com>
2024-12-19 15:46:50 -0600

    Remove unused setting

--- settings.example.py
+++ settings.example.py
@@ -4,4 +4,3 @@ NTP_HOST = 'pool.ntp.org'
 APP_ID = ''
 BATTERY_LOW = 1660 # 3.4v; Calibrated using benchtop supply
 BATTERY_HIGH = 2180 # 4.2v; Calibrated using benchtop supply
-TZ_OFFSET = 7 * 60 * 60
